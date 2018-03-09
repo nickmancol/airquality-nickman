9d3c6fcd53586c24fcec7c379507cc802793ef80
Nicolas Bohorquez <nicolas.bohorquez@alephsa.com>
2018-03-09 13:36:26 +0100

    Adds basisc notebook to merge data files

--- lab1.py
+++ lab1.py
@@ -11,20 +11,22 @@ def merge_data(folder='./', preffix='mi_pollution'):
         return
 
     dataframes = []
-    df_names = pd.DataFrame()
+    names = pd.DataFrame()
     for filename in os.listdir(folder):
         if filename.startswith(preffix) and filename.endswith(".csv"):
-            dataframes.append(pd.read_csv(folder+filename))
 
+            dataframes.append(pd.read_csv(folder+filename, header=None))
         if 'legend' in filename and filename.endswith(".csv"):
-            df_names = pd.read_csv(folder+filename)
+            names = pd.read_csv(folder+filename, header=None)
 
-    df_total = pd.DataFrame()
+    total = pd.DataFrame()
     for df in dataframes:
-        df_total = df_total + df
-    #TODO: merge with legend and plot
-    print(np.shape(df))
+        total = total.append(df)
 
+    total.join(names, lsuffix='_total', rsuffix='_names')
+    
+    print(total.head())
+    print(np.shape(total))
 
 if __name__ != "main":
     merge_data('/home/nickman/code/MADAS/II/Lab Rossi/MI_Air_Quality/data/')
